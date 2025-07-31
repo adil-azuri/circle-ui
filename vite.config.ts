2456a1cdf8c42299fb71814950c68405f611539b
Adil Aulia Azuri <adil.azuri145@gmail.com>
2025-07-31 20:00:49 +0700

    setup vite config for deploy

--- vite.config.ts
+++ vite.config.ts
@@ -1,15 +1,8 @@
 import { defineConfig } from 'vite'
-import tsconfigPaths from "vite-tsconfig-paths"
 import react from '@vitejs/plugin-react'
-import path from "path"
-import tailwindcss from "@tailwindcss/vite"
+import tsconfigPaths from 'vite-tsconfig-paths'
 
-// https://vite.dev/config/
+// https://vitejs.dev/config/
 export default defineConfig({
-  plugins: [react(), tailwindcss(), tsconfigPaths()],
-  resolve: {
-    alias: {
-      "@": path.resolve(__dirname, "./src"),
-    },
-  },
+  plugins: [react(), tsconfigPaths()],
 })
