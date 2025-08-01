30da2622ea3cb16fc1a2bf492097367d3de647bb
Adil Aulia Azuri <adil.azuri145@gmail.com>
2025-08-01 11:23:54 +0700

    fix url api again

--- src/api/api.ts
+++ src/api/api.ts
@@ -1,6 +1,8 @@
 import axios from "axios";
 
+const baseURL = import.meta.env.VITE_API_URL || "https://circle-api-adil.vercel.app/api/v1";
+
 export const api = axios.create({
-    baseURL: "https://circle-api-adil.vercel.app/api/v1",
+    baseURL: baseURL,
     withCredentials: true,
 });
