7b93b703b77cfc1f7ac021a4884a19160e18fded
Adil Aulia Azuri <adil.azuri145@gmail.com>
2025-08-01 14:25:32 +0700

    change auth context get token from localstorage

--- src/context/authContext.tsx
+++ src/context/authContext.tsx
@@ -1,19 +1,18 @@
 import type { AuthContextType } from "@/types/AuthContextType";
 import { createContext, useState } from "react";
-import Cookies from "js-cookie";
 
 export const AuthContext = createContext<AuthContextType | null>(null);
 
 export function AuthProvider({ children }: { children: React.ReactNode }) {
-    const [token, setToken] = useState<string | null>(() => Cookies.get("token") || null);
+    const [token, setToken] = useState<string | null>(() => localStorage.getItem("token") || null);
 
     const auth = (token: string) => {
-        Cookies.set("token", token);
+        localStorage.setItem("token", token);
         setToken(token);
     };
 
     const logout = () => {
-        Cookies.remove("token");
+        localStorage.removeItem("token");
         setToken(null);
     };
 
