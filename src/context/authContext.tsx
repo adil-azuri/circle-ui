e5baaa2f1eac6a2ba9fba8da4c7ecdb5e3561334
Adil Aulia Azuri <adil.azuri145@gmail.com>
2025-08-01 15:09:11 +0700

    fix auth context

--- src/context/authContext.tsx
+++ src/context/authContext.tsx
@@ -1,23 +1,17 @@
 import type { AuthContextType } from "@/types/AuthContextType";
-import { createContext, useState } from "react";
+import { createContext } from "react";
 
 export const AuthContext = createContext<AuthContextType | null>(null);
 
 export function AuthProvider({ children }: { children: React.ReactNode }) {
-    const [token, setToken] = useState<string | null>(() => localStorage.getItem("token") || null);
-
-    const auth = (token: string) => {
-        localStorage.setItem("token", token);
-        setToken(token);
+    const auth = () => {
     };
 
     const logout = () => {
-        localStorage.removeItem("token");
-        setToken(null);
     };
 
     return (
-        <AuthContext.Provider value={{ token, auth, logout }}>
+        <AuthContext.Provider value={{ token: null, auth, logout }}>
             {children}
         </AuthContext.Provider>
     );
